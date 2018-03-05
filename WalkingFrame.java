87e31da3892e166bcbd287088ee98fd5fd7facb4
Karl J Ahlqvist <36700017+KarlAhlqvist18@users.noreply.github.com>
2018-03-05 08:00:04 -0500

    Add files via upload

---
+++ WalkingFrame.java
@@ -0,0 +1,120 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class WalkingFrame extends JFrame implements ActionListener
+{
+	private Man man;
+	ArrayList<Ball> balls = new ArrayList<Ball>();
+	public WalkingFrame()
+	{
+		
+		setBounds(100,100,600,400);
+		setLayout(null);
+		man = new Man(0,0);
+		add(man);
+		Timer timer = new Timer(10,this);
+		timer.start();
+		addKeyListener(new KeyListener()
+				{
+
+					@Override
+					public void keyPressed(KeyEvent e) 
+					{
+						if(e.getKeyCode() == e.VK_W)
+						{
+							man.setDY(-3);
+						}
+						
+						if(e.getKeyCode() == e.VK_S)
+						{
+							man.setDY(3);
+						}
+						
+						if(e.getKeyCode() == e.VK_A)
+						{
+							man.setDX(-3);
+						}
+					
+						if(e.getKeyCode() == e.VK_D)
+						{
+							man.setDX(3);
+						}
+						
+						if(e.getKeyCode() == e.VK_SPACE)
+						{
+							Ball ball = new Ball(man.getX(),man.getY());
+							add(ball);
+							balls.add(ball);
+							ball.setDX(5);
+						}
+						
+			
+					}
+					
+
+					@Override
+					public void keyReleased(KeyEvent e) 
+					{
+						if(e.getKeyCode() == e.VK_W)
+						{
+							man.setDY(0);
+						}
+						
+						if(e.getKeyCode() == e.VK_S)
+						{
+							man.setDY(0);
+						}
+						
+						if(e.getKeyCode() == e.VK_A)
+						{
+							man.setDX(0);
+						}
+					
+						if(e.getKeyCode() == e.VK_D)
+						{
+							man.setDX(0);
+						}
+						
+						
+					}
+
+					@Override
+					public void keyTyped(KeyEvent e) 
+					{
+						
+					}
+					
+				});
+		setVisible(true);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args)
+	{
+		new WalkingFrame();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		man.update();
+		for(int i = 0; i < balls.size(); i++)
+		{
+			balls.get(i).update();
+			if(balls.get(i).getX() >= 600)
+			{
+				this.remove(balls.get(i));
+				balls.remove(i);
+			}
+		}
+		repaint();
+	}
+	
+}
